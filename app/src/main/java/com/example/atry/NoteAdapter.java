aaaf4a1889e555b830f6f4b6f9ccacdc5aa85024
hxazy <3537994247@qq.com>
2024-11-17 02:52:53 +0800

    Update NoteAdapter.java

--- app/src/main/java/com/example/atry/NoteAdapter.java
+++ app/src/main/java/com/example/atry/NoteAdapter.java
@@ -18,8 +18,8 @@ public class NoteAdapter extends BaseAdapter implements Filterable {
 
     private Context mContext;
 
-    private List<Note> backList;//用来备份原始数据
-    private List<Note> noteList;//这个数据是会改变的，所以要有个变量来备份一下原始数据
+    private List<Note> backList;
+    private List<Note> noteList;
     private MyFilter mFilter;
 
     public NoteAdapter(Context mContext, List<Note> noteList) {
@@ -51,14 +51,12 @@ public class NoteAdapter extends BaseAdapter implements Filterable {
         TextView tv_content = (TextView)v.findViewById(R.id.tv_content);
         TextView tv_time = (TextView)v.findViewById(R.id.tv_time);
 
-        //Set text for TextView
         String allText = noteList.get(position).getContent();
         if (sharedPreferences.getBoolean("noteTitle" ,true))
             tv_content.setText(allText.split("\n")[0]);
         else tv_content.setText(allText);
         tv_time.setText(noteList.get(position).getTime());
 
-        //Save note id to tag
         v.setTag(noteList.get(position).getId());
 
         return v;
@@ -74,37 +72,33 @@ public class NoteAdapter extends BaseAdapter implements Filterable {
 
 
     class MyFilter extends Filter {
-        //我们在performFiltering(CharSequence charSequence)这个方法中定义过滤规则
         @Override
         protected FilterResults performFiltering(CharSequence charSequence) {
             FilterResults result = new FilterResults();
             List<Note> list;
-            if (TextUtils.isEmpty(charSequence)) {//当过滤的关键字为空的时候，我们则显示所有的数据
+            if (TextUtils.isEmpty(charSequence)) {
                 list = backList;
-            } else {//否则把符合条件的数据对象添加到集合中
+            } else {
                 list = new ArrayList<>();
                 for (Note note : backList) {
                     if (note.getContent().contains(charSequence)) {
                         list.add(note);
                     }
-
                 }
             }
-            result.values = list; //将得到的集合保存到FilterResults的value变量中
-            result.count = list.size();//将集合的大小保存到FilterResults的count变量中
+            result.values = list;
+            result.count = list.size();
 
             return result;
         }
-        //在publishResults方法中告诉适配器更新界面
         @Override
         protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
             noteList = (List<Note>)filterResults.values;
             if (filterResults.count>0){
-                notifyDataSetChanged();//通知数据发生了改变
+                notifyDataSetChanged();
             }else {
-                notifyDataSetInvalidated();//通知数据失效
+                notifyDataSetInvalidated();
             }
         }
     }
-
 }
